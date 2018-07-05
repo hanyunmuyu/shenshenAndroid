c5c921d19a19c23217bbe909d7106bd6f0212700
hanyun <1355081829@qq.com>
2018-07-05 18:08:29 +0800

    init

--- app/src/main/java/com/example/administrator/myapplication/school/HomeFragment.java
+++ app/src/main/java/com/example/administrator/myapplication/school/HomeFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.ArrayMap;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +15,7 @@ import com.example.administrator.myapplication.school.adapter.HomeRecyclerViewAd
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -33,12 +35,19 @@ public class HomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_school_home, container, false);
         recyclerView = view.findViewById(R.id.rv);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        List<String> list = new ArrayList<>();
-        for (int i = 0; i < 15; i++) {
-            list.add("" + i);
-        }
 
-        HomeRecyclerViewAdapter homeRecyclerViewAdapter = new HomeRecyclerViewAdapter(list, getContext());
+        List<Map<String, Object>> mapArrayList = new ArrayList<>();
+        for (int i = 0; i < 16; i++) {
+            Map<String, Object> map = new ArrayMap<>();
+            map.put("pic", "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1530787601738&di=fadc5579bcd1e747db056df10cea82d4&imgtype=0&src=http%3A%2F%2Fimg.pconline.com.cn%2Fimages%2Fupload%2Fupc%2Ftx%2Fitbbs%2F1408%2F18%2Fc23%2F37570958_1408354006609_mthumb.jpg");
+            map.put("name", "PLMM");
+            map.put("title", "这个MM真靓");
+            map.put("content", "这样的MM有谁喜欢呢？送你一打！哈哈哈哈");
+            map.put("favoriteNumber", 100);
+            map.put("collectionNumber", 1000);
+            mapArrayList.add(map);
+        }
+        HomeRecyclerViewAdapter homeRecyclerViewAdapter = new HomeRecyclerViewAdapter(mapArrayList, getContext());
         recyclerView.setAdapter(homeRecyclerViewAdapter);
 
         return view;
--- app/src/main/java/com/example/administrator/myapplication/school/adapter/HomeRecyclerViewAdapter.java
+++ app/src/main/java/com/example/administrator/myapplication/school/adapter/HomeRecyclerViewAdapter.java
@@ -5,44 +5,57 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.administrator.myapplication.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class HomeRecyclerViewAdapter extends RecyclerView.Adapter<HomeRecyclerViewAdapter.MyViewHolder> {
-    private List<String> stringList;
+    private List<Map<String, Object>> mapArrayList;
     private Context context;
 
-    public HomeRecyclerViewAdapter(List<String> stringList, Context context) {
-        this.stringList = stringList;
+    public HomeRecyclerViewAdapter(List<Map<String, Object>> mapArrayList, Context context) {
+        this.mapArrayList = mapArrayList;
         this.context = context;
     }
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         MyViewHolder myViewHolder = new MyViewHolder(LayoutInflater.from(context).inflate(R.layout.school_home_item, parent, false));
-
         return myViewHolder;
     }
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-        holder.textView.setText(stringList.get(position));
+        Map<String, Object> map = mapArrayList.get(position);
+        Picasso.get().load((String) map.get("pic")).into(holder.pic);
+        holder.name.setText((String) map.get("name"));
+        holder.title.setText((String) map.get("title"));
     }
 
     @Override
     public int getItemCount() {
-        return stringList.size();
+        return mapArrayList.size();
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
-        public TextView textView;
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        public ImageView pic;
+        public TextView name;
+        public TextView title;
+        public TextView content;
 
         public MyViewHolder(View itemView) {
             super(itemView);
-            textView = itemView.findViewById(R.id.tv);
+            pic = itemView.findViewById(R.id.pic);
+            name = itemView.findViewById(R.id.name);
+            title = itemView.findViewById(R.id.title);
         }
     }
 }
