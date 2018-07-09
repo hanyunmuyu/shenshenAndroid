abdd7fa7ccf25631ca44f1b6712aeb6e9b4b4636
hanyun <1355081829@qq.com>
2018-07-09 10:40:22 +0800

    init

--- app/src/main/java/com/example/administrator/myapplication/fragment/MeFragment.java
+++ app/src/main/java/com/example/administrator/myapplication/fragment/MeFragment.java
@@ -1,32 +1,38 @@
 package com.example.administrator.myapplication.fragment;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.example.administrator.myapplication.MainActivity;
 import com.example.administrator.myapplication.R;
-import com.uuzuche.lib_zxing.activity.CaptureActivity;
 import com.uuzuche.lib_zxing.activity.CodeUtils;
-import com.uuzuche.lib_zxing.activity.ZXingLibrary;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MeFragment extends Fragment {
+    Button logout;
     private ImageView imageView;
     private ImageView iv;
 
     public MeFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,6 +42,9 @@ public class MeFragment extends Fragment {
 //        Intent intent = new Intent(getActivity().getApplicationContext(), CaptureActivity.class);
 //        startActivityForResult(intent, 1);
         View view = inflater.inflate(R.layout.fragment_me, container, false);
+
+        ButterKnife.bind(this, view);
+
         imageView = view.findViewById(R.id.capture);
         iv = view.findViewById(R.id.iv);
         imageView.setOnClickListener(new View.OnClickListener() {
@@ -48,4 +57,9 @@ public class MeFragment extends Fragment {
         return view;
     }
 
+    @OnClick(R.id.logout)
+    public void onClick(View v) {
+        MainActivity mainActivity = (MainActivity) getActivity();
+        mainActivity.logout();
+    }
 }
---
+++ app/src/main/java/com/example/administrator/myapplication/lib/Token.java
@@ -0,0 +1,4 @@
+package com.example.administrator.myapplication.lib;
+
+public class Token {
+}
