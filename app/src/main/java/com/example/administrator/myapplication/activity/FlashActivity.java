2bd4ef2d5765fbe0633770595cb4ea3dc789462d
hanyun <1355081829@qq.com>
2018-07-16 22:27:51 +0800

    init

--- app/src/main/java/com/example/administrator/myapplication/activity/FlashActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/activity/FlashActivity.java
@@ -4,22 +4,43 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
 import com.example.administrator.myapplication.MainActivity;
 import com.example.administrator.myapplication.R;
+import com.jph.takephoto.app.TakePhotoActivity;
+import com.jph.takephoto.model.TResult;
 
-public class FlashActivity extends Activity {
+public class FlashActivity extends TakePhotoActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(FlashActivity.this, MainActivity.class));
-                finish();
-            }
-        }, 300);
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                startActivity(new Intent(FlashActivity.this, MainActivity.class));
+//                finish();
+//            }
+//        }, 300);
+        getTakePhoto().onPickMultiple(3);
+
     }
+
+    @Override
+    public void takeSuccess(TResult result) {
+        result.getImages();
+    }
+
+    @Override
+    public void takeFail(TResult result, String msg) {
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void takeCancel() {
+        super.takeCancel();
+    }
+
 }
