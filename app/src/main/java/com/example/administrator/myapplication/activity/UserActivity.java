429c0d39bde5b2a6e5d2449026dd1989b3dd7cc5
hanyun <1355081829@qq.com>
2018-08-02 10:34:45 +0800

    编辑用户信息

---
+++ app/src/main/java/com/example/administrator/myapplication/activity/UserActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.myapplication.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.administrator.myapplication.R;
+
+public class UserActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
