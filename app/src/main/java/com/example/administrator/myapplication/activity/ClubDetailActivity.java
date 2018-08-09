5ca273a621d80a10343be7d74132aaaefa1e7c6e
hanyun <1355081829@qq.com>
2018-08-09 15:02:05 +0800

    关注社团

--- app/src/main/java/com/example/administrator/myapplication/activity/ClubDetailActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/activity/ClubDetailActivity.java
@@ -3,11 +3,13 @@ package com.example.administrator.myapplication.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.administrator.myapplication.MainActivity;
 import com.example.administrator.myapplication.R;
 import com.example.administrator.myapplication.model.QrCodeModel;
 import com.example.administrator.myapplication.myView.NineImageview;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +22,8 @@ public class ClubDetailActivity extends Activity {
 
     @BindView(R.id.nineView)
     public NineImageview mNineImageview;
-
+    @BindView(R.id.logo)
+    public ImageView logo;
 
     private int clubId;
 
@@ -43,6 +46,10 @@ public class ClubDetailActivity extends Activity {
         list.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1533727999816&di=e7525b5077067da3326990fda7998281&imgtype=0&src=http%3A%2F%2Fwww.officedoyen.com%2Fuploads%2Fallimg%2F140929%2F1-140929230255201.jpg");
         list.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1533727999815&di=e1a5bf060bb5ead19ba8055508a0bdec&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimage%2Fc0%253Dpixel_huitu%252C0%252C0%252C294%252C40%2Fsign%3Dc5f68d1bb81bb0519b29bb685f02bfd8%2F10dfa9ec8a1363270e51430a9a8fa0ec08fac74f.jpg");
         mNineImageview.setStringList(list).setLineNumber(3);
+
+
+        Picasso.get().load(R.mipmap.dog).fit().centerCrop().into(logo);
+
     }
 
     @OnClick(R.id.qr)
@@ -56,5 +63,10 @@ public class ClubDetailActivity extends Activity {
         bundle.putSerializable("code", qrCodeModel);
         intent.putExtras(bundle);
         startActivity(intent);
+    }
+
+    @OnClick(R.id.backBtn)
+    public void back() {
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/administrator/myapplication/model/ClubModel.java
@@ -0,0 +1,10 @@
+package com.example.administrator.myapplication.model;
+
+public class ClubModel {
+    private String mLogo;
+    private String mTitle;
+    private int mAttentionNumver = 0;
+    private int mPostNumber = 0;
+    private int mMemberNumber = 0;
+
+}
